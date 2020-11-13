f54f914e93d3199cfc2db03543be3cead1238eaf
Matthias Urlichs <matthias@urlichs.de>
2020-11-13 08:39:58 +0100

    Call msgfmt to build i18n files

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import xml.sax.saxutils
 from os.path import join
 import sys
 import os
+import glob
+import pathlib
+import subprocess
 
 def get_version(fname='formalchemy/__init__.py'):
     with open(fname) as f:
@@ -22,6 +25,20 @@ long_description = '.. contents:: :depth: 1\n\n' +\
                    '\n\n' +\
                    read('CHANGELOG.rst')
 
+PO_FILES = 'i18n_resources/*/LC_MESSAGES/formalchemy.po'
+
+def create_mo_files():
+    mo_files = []
+    prefix = 'formalchemy'
+
+    for po_path in glob.glob(str(pathlib.Path(prefix) / PO_FILES)):
+        mo = pathlib.Path(po_path.replace('.po', '.mo'))
+
+        subprocess.run(['msgfmt', '-o', str(mo), po_path], check=True)
+        mo_files.append(str(mo.relative_to(prefix)))
+
+    return mo_files
+
 setup(name='FormAlchemy',
       license='MIT License',
       version=get_version(),
@@ -36,7 +53,7 @@ setup(name='FormAlchemy',
                                     'ext/pylons/*.mako', 'ext/pylons/resources/*.css', 'ext/pylons/resources/*.png',
                                     'tests/data/mako/*.mako', 'tests/data/genshi/*.html',
                                     'paster_templates/pylons_fa/+package+/*/*_tmpl',
-                                    ]},
+                                    ] + create_mo_files()},
       include_package_data=True,
       classifiers=[
           'Development Status :: 5 - Production/Stable',
